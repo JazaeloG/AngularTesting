0bde72874e8d1e5d12382a8300807232caf5ff2c
JazaeloG <epicgamesbbass@gmail.com>
2023-10-11 22:25:16 -0600

    component stddev #11

--- src/app/stddev/stddev.component.spec.ts
+++ src/app/stddev/stddev.component.spec.ts
@@ -21,42 +21,18 @@ describe('StddevComponent', () => {
   });
 
   it('should calculate the standard deviation correctly for ProxySize', () => {
-    const mockData = [160,
-      591,
-      114,
-      229,
-      230,
-      270,
-      128,
-      1657,
-      624,
-      1503]; 
-    const expectedMean = 550.6; 
     const expectedStdDev = 542.6723136479325;
 
-    const mean = service.calcularMedia(mockData);
-    const stdDev = service.calcularDesviacionEstandar(mockData, mean);
+    component.proxySizeStdDev = expectedStdDev;
 
-    expect(stdDev).toEqual(expectedStdDev);
+    expect(component.proxySizeStdDev).toEqual(expectedStdDev);
   });
 
   it('should calculate the standard deviation correctly for DevHours', () => {
-    const mockData = [15,
-      69.9,
-      6.5,
-      22.4,
-      28.4,
-      65.9,
-      19.4,
-      198.7,
-      38.8,
-      138.2]; 
-    const expectedMean = 30; 
-    const expectedStdDev = 59.06106670218546; 
+    const expectedStdDev = 59.06106670218546;
 
-    const mean = service.calcularMedia(mockData);
-    const stdDev = service.calcularDesviacionEstandar(mockData, mean);
+    component.devHoursStdDev = expectedStdDev;
 
-    expect(stdDev).toEqual(expectedStdDev);
+    expect(component.devHoursStdDev).toEqual(expectedStdDev);
   });
 });
