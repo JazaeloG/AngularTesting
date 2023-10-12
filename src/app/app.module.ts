b27a67ef10a242081072e892612830a9d2239cec
Jazael Galindo <115960860+JazaeloG@users.noreply.github.com>
2023-10-11 21:52:07 -0600

    Media Close (#10)
    * media #9
    
    * component media #9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MediaComponent } from './media/media.component';
+import { MediasService } from './services/medias.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MediaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [MediasService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
