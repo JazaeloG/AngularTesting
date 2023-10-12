8566da0ac51a48e5ae67421e3a8b1c28b7c44f26
JazaeloG <epicgamesbbass@gmail.com>
2023-10-11 21:50:02 -0600

    component media #9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MediaComponent } from './media/media.component';
+import { MediasService } from './services/medias.service';
 
 @NgModule({
   declarations: [
@@ -12,9 +13,10 @@ import { MediaComponent } from './media/media.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [MediasService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/media/media.component.spec.ts
+++ src/app/media/media.component.spec.ts
@@ -1,31 +1,41 @@
-// media.spec.ts
-
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MediaComponent } from './media.component';
+import { MediasService } from '../services/medias.service';
+import { HttpClientModule } from '@angular/common/http';
+import { of } from 'rxjs';
 
 describe('MediaComponent', () => {
   let component: MediaComponent;
   let fixture: ComponentFixture<MediaComponent>;
+  let mediaService: MediasService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [MediaComponent],
-    });
+      providers: [MediasService],
+      imports: [HttpClientModule]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(MediaComponent);
     component = fixture.componentInstance;
+    mediaService = TestBed.inject(MediasService);
   });
 
-  it('should return the correct average of column 1', () => {
-    const data = [1, 2, 3, 4, 5];
-    const expectedMedia = 3;
-    const actualMedia = calculateMedia(data);
-    expect(actualMedia).toBe(expectedMedia);
+  it('Should return mean=60.32 with the json 1a_dev_hours', () => {
+    const devHoursData = [15, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2];
+    spyOn(mediaService, 'getDevHours').and.returnValue(of(devHoursData));
+
+    fixture.detectChanges(); 
+
+    expect(component.devHoursAverage).toBeCloseTo(60.32, 2);
   });
 
-  it('should return the correct average of column 2', () => {
-    const data = [2, 4, 6, 8, 10];
-    const expectedMedia = 6;
-    const actualMedia = calculateMedia(data);
-    expect(actualMedia).toBe(expectedMedia);
-  })
+  it('Should return mean=550.6 with the json 1a_proxy_size', () => {
+    const proxySizeData = [160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503];
+    spyOn(mediaService, 'getProxySize').and.returnValue(of(proxySizeData));
+
+    fixture.detectChanges();
+
+    expect(component.proxySizeAverage).toBeCloseTo(550.6, 1);
+  });
 });
