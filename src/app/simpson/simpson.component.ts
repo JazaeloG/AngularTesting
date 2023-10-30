ee2ae33e0c14c467ff0d32b2c613989fc8ceb210
JazaeloG <epicgamesbbass@gmail.com>
2023-10-30 15:28:49 -0600

    simpson

--- src/app/simpson/simpson.component.ts
+++ src/app/simpson/simpson.component.ts
@@ -8,16 +8,17 @@ import { Calculate } from '../operations/calculos';
 })
 export class SimpsonComponent {
   calculate = new Calculate();
+
   calcularArea(fx: any, x0: number, x1: number, seg: number, error: number): any {
-    
     let a1 = 0;
     let a2 = 0;
     let c = 1;
-    while (a1 == 0 || a2 - a1 > error || a2 == 0) {
-      if (c == 1) {
+
+    while (a1 === 0 || a2 - a1 > error || a2 === 0) {
+      if (c === 1) {
         a1 = this.simpson(seg, fx, x0, x1);
       } else {
-        if (a2 != 0) {
+        if (a2 !== 0) {
           a1 = a2;
         }
         seg *= 2;
@@ -25,16 +26,20 @@ export class SimpsonComponent {
       }
       c += 1;
     }
+
     return a2;
   }
-  
+
   simpson(segmentos: any, fx: any, x0: any, x1: any): number {
     const w = (x1 - x0) / segmentos;
     let suma = this.calculate.calcularOperacion(fx, x0);
+
     for (let i = 1; i < segmentos; i++) {
       const multiplo = i % 2 === 0 ? 2 : 4;
       suma += multiplo * this.calculate.calcularOperacion(fx, w * i + x0);
     }
-    suma += this.calculate.calcularOperacion(fx, x1);
 
+    suma += this.calculate.calcularOperacion(fx, x1);
+    return (w / 3) * suma;
+  }
 }
