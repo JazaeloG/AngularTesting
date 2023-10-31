96155a0dcd261c8295a7f6828b03ae171d41f803
JazaeloG <epicgamesbbass@gmail.com>
2023-10-30 22:07:14 -0600

    changes to simpson

--- src/app/simpson/simpson.component.ts
+++ src/app/simpson/simpson.component.ts
@@ -9,35 +9,48 @@ import { Calculate } from '../operations/calculos';
 export class SimpsonComponent {
   calculate = new Calculate();
 
-  calcularAreaSimpson(fx: any, x0: number, x1: number, seg: number, error: number): any {
+  calcularAreaSimpson(fx: (x: number) => number, x0: number, x1: number, seg: number, error: number): number {
     let a1 = 0;
     let a2 = 0;
     let c = 1;
-     while (a1 === 0 || a2 - a1 > error || a2 === 0) {
+
+    while (a1 === 0 || Math.abs(a2 - a1) > error || a2 === 0) {
       if (c === 1) {
-        a1 = this.calculoSimpson(seg, fx, x0, x1);
+        a1 = this.simpson(fx, x0, x1, seg);
       } else {
         if (a2 !== 0) {
           a1 = a2;
         }
         seg *= 2;
-        a2 = this.calculoSimpson(seg, fx, x0, x1);
+        a2 = this.simpson(fx, x0, x1, seg);
       }
       c += 1;
     }
     return a2;
   }
 
-  calculoSimpson(segmentos: any, fx: any, x0: any, x1: any): number {
-    const w = (x1 - x0) / segmentos;
-    let suma = this.calculate.calcularX(fx, x0);
+  simpson(fx: (x: number) => number, x0: number, x1: number, seg: number): number {
+    const w = (x1 - x0) / seg;
+    let suma = fx(x0) + fx(x1);
 
-    for (let i = 1; i < segmentos; i++) {
+    for (let i = 1; i < seg; i++) {
       const multiplo = i % 2 === 0 ? 2 : 4;
-      suma += multiplo * this.calculate.calcularX(fx, w * i + x0);
+      suma += multiplo * fx(x0 + i * w);
     }
 
-    suma += this.calculate.calcularX(fx, x1);
     return (w / 3) * suma;
+  }
+
+  
+  f2x(x: number): number {
+    return 2 * x;
+  }
+
+  fx2(x: number): number {
+    return x * x;
+  }
+
+  f1_x(x: number): number {
+    return 1 / x;
   }
 }
